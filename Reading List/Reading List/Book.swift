5d1a48fbe3e9a54441026abd2246567db04ee762
thoaile <thoaimlevn@gmail.com>
2019-06-11 13:45:46 -0700

    First Draft

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,31 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Thoai Le on 6/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    let title: String
+    let reasonToRead: String
+    let hasBeenRead: Bool
+    
+    init(hasBeenRead: String? = false)
+    
+    self.title = title
+    self.reasonToRead = reasonToRead
+    seft.hasBeenRead = hasBeenRead
+    
+    protocol equatable {
+        <#requirements#>
+        
+    protocol codable {
+            <#requirements#>
+        }
+    }
+    
+}
+
---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,13 @@
+//
+//  BookController.swift
+//  
+//
+//  Created by Thoai Le on 6/11/19.
+//
+
+import Foundation
+
+class BookController
+var books = Book[]
+
+
